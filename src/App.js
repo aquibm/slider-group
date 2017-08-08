ddda0792460a5cdb390ff3e93c90926808821a0b
Aquib Master <aquibmaster@gmail.com>
2017-08-08 20:10:39 +1200

    (spike): Balance the sliders

--- src/App.js
+++ src/App.js
@@ -6,66 +6,79 @@ import 'rc-slider/assets/index.css'
 
 class App extends Component {
     state = {
-        sliderValue: 0
+        sliders: [0.25, 0.5, 0.25]
     }
 
-    _handleChange = sliderValue =>
-        this.setState({
-            sliderValue
-        })
+    _handleChange = (index, newValue) => {
+        const oldTotalExcludingCurrent = this.state.sliders
+            .filter((val, idx) => idx !== index)
+            .reduce((prev, curr) => prev + curr, 0)
+
+        const newTotalExcludingCurrent = 1 - newValue
+
+        const numberOfSlidersToModify = this.state.sliders.length - 1
+
+        this.setState(state => ({
+            sliders: [
+                ...state.sliders
+                    .slice(0, index)
+                    .map(value =>
+                        this.computeProportionalValue(
+                            value,
+                            oldTotalExcludingCurrent,
+                            newTotalExcludingCurrent,
+                            numberOfSlidersToModify
+                        )
+                    ),
+                newValue,
+                ...state.sliders
+                    .slice(index + 1, state.sliders.length)
+                    .map(value =>
+                        this.computeProportionalValue(
+                            value,
+                            oldTotalExcludingCurrent,
+                            newTotalExcludingCurrent,
+                            numberOfSlidersToModify
+                        )
+                    )
+            ]
+        }))
+    }
+
+    computeProportionalValue(
+        value,
+        oldTotalExcludingCurrent,
+        newTotalExcludingCurrent,
+        numberOfSlidersToModify
+    ) {
+        if (oldTotalExcludingCurrent === 0)
+            return newTotalExcludingCurrent / numberOfSlidersToModify
+
+        return value / oldTotalExcludingCurrent * newTotalExcludingCurrent || 0
+    }
 
     render() {
-        const { sliderValue } = this.state
+        const { sliders } = this.state
 
         return (
             <div className="app">
-                <div className="slider-wrap">
-                    <Slider
-                        vertical
-                        min={0}
-                        max={1}
-                        value={sliderValue}
-                        onChange={this._handleChange}
-                        defaultValue={0}
-                        step={0.001}
-                    />
+                {sliders.map((value, idx) =>
+                    <div key={idx} className="slider-wrap">
+                        <Slider
+                            vertical
+                            min={0}
+                            max={1}
+                            value={value}
+                            onChange={newValue =>
+                                this._handleChange(idx, newValue)}
+                            step={0.001}
+                        />
 
-                    <p className="slider-value">
-                        {sliderValue}
-                    </p>
-                </div>
-
-                <div className="slider-wrap">
-                    <Slider
-                        vertical
-                        min={0}
-                        max={1}
-                        value={sliderValue}
-                        onChange={this._handleChange}
-                        defaultValue={0}
-                        step={0.001}
-                    />
-
-                    <p className="slider-value">
-                        {sliderValue}
-                    </p>
-                </div>
-
-                <div className="slider-wrap">
-                    <Slider
-                        vertical
-                        min={0}
-                        max={1}
-                        value={sliderValue}
-                        onChange={this._handleChange}
-                        defaultValue={0}
-                        step={0.001}
-                    />
-
-                    <p className="slider-value">
-                        {sliderValue}
-                    </p>
-                </div>
+                        <p className="slider-value">
+                            {Math.round(value * 100) / 100}
+                        </p>
+                    </div>
+                )}
             </div>
         )
     }
