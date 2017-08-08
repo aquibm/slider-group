2b03a6002074793e9b1412ada6e66fefcab62ee4
Aquib Master <aquibmaster@gmail.com>
2017-08-08 18:51:55 +1200

    Get some sliders going

--- src/App.js
+++ src/App.js
@@ -1,9 +1,73 @@
 import React, { Component } from 'react'
+import Slider from 'rc-slider'
+
 import './App.css'
+import 'rc-slider/assets/index.css'
 
 class App extends Component {
+    state = {
+        sliderValue: 0
+    }
+
+    _handleChange = sliderValue =>
+        this.setState({
+            sliderValue
+        })
+
     render() {
-        return <div>Hello world!</div>
+        const { sliderValue } = this.state
+
+        return (
+            <div className="app">
+                <div className="slider-wrap">
+                    <Slider
+                        vertical
+                        min={0}
+                        max={1}
+                        value={sliderValue}
+                        onChange={this._handleChange}
+                        defaultValue={0}
+                        step={0.001}
+                    />
+
+                    <p className="slider-value">
+                        {sliderValue}
+                    </p>
+                </div>
+
+                <div className="slider-wrap">
+                    <Slider
+                        vertical
+                        min={0}
+                        max={1}
+                        value={sliderValue}
+                        onChange={this._handleChange}
+                        defaultValue={0}
+                        step={0.001}
+                    />
+
+                    <p className="slider-value">
+                        {sliderValue}
+                    </p>
+                </div>
+
+                <div className="slider-wrap">
+                    <Slider
+                        vertical
+                        min={0}
+                        max={1}
+                        value={sliderValue}
+                        onChange={this._handleChange}
+                        defaultValue={0}
+                        step={0.001}
+                    />
+
+                    <p className="slider-value">
+                        {sliderValue}
+                    </p>
+                </div>
+            </div>
+        )
     }
 }
 
